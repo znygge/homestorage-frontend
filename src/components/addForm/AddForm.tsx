f72b827527b4238caa55a27e1da3d85314c325e9
Johan Nygren <johan.nygren@knowit.se>
2022-10-15 13:13:34 +0200

    Updated store of data

--- src/components/addForm/AddForm.tsx
+++ src/components/addForm/AddForm.tsx
@@ -2,22 +2,22 @@ import React, { FC, useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
-import { Units } from "../../datastructures";
+import { NewProduct, Product, Units } from "../../datastructures";
 import { WithContext as ReactTags } from "react-tag-input";
 
 interface AddFormProps {
   show?: boolean;
 }
 interface Tag {
-  id: string,
-  text: string
+  id: string;
+  text: string;
 }
 
 const AddForm: FC<AddFormProps> = (props: AddFormProps) => {
   const [show, setShow] = useState(false);
   const [tags, setTags] = React.useState([] as Array<Tag>);
-  const [name, setName] = React.useState('');
-  const [location, setLocation] = React.useState('');
+  const [name, setName] = React.useState("");
+  const [location, setLocation] = React.useState("");
   const [amount, setAmount] = React.useState(0);
   const [unit, setUnit] = React.useState(Units.st);
 
@@ -26,13 +26,46 @@ const AddForm: FC<AddFormProps> = (props: AddFormProps) => {
   };
 
   const handleSubmit = () => {
-     
-    console.log(name, location, amount, unit, tags)
-    handleClose();
+    console.log(name, location, amount, unit, tags);
+    const item: NewProduct = {
+      name,
+      location,
+      amount,
+      unit,
+      tags: tags.map((tag) => tag.text),
+    };
+    fetch("http://server.nygge.eu:4000/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `mutation {
+singleCreateItem(record: {
+name: "${item.name}"
+location:"${item.location}",
+amount: ${item.amount},
+unit: "${item.unit}",
+tags: ${JSON.stringify(item.tags)}}) 
+{
+record {
+name
+location
+amount
+unit
+category,
+tags
+}
+}
+}`
+      }),
+    }).then((resp) => {
+      handleClose();
+    });
   };
   const handleClose = () => {
-    setName('');
-    setLocation('');
+    setName("");
+    setLocation("");
     setAmount(0);
     setUnit(Units.st);
     setTags([] as Array<Tag>);
@@ -46,7 +79,7 @@ const AddForm: FC<AddFormProps> = (props: AddFormProps) => {
   const handleAddition = (tag: Tag) => {
     setTags([...tags, tag]);
   };
-  
+
   return (
     <span>
       <Button className="p-2" variant="success" onClick={handleShow}>
@@ -60,19 +93,37 @@ const AddForm: FC<AddFormProps> = (props: AddFormProps) => {
           <Form>
             <Form.Group className="mb-3" controlId="name">
               <Form.Label>Namn</Form.Label>
-              <Form.Control type="text" placeholder="Toalettpapper" value={name} onChange={e => setName(e.target.value)}/>
+              <Form.Control
+                type="text"
+                placeholder="Toalettpapper"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="location" >
+            <Form.Group className="mb-3" controlId="location">
               <Form.Label>Plats</Form.Label>
-              <Form.Control type="text" placeholder="Kylskåp" value={location} onChange={e => setLocation(e.target.value)}/>
+              <Form.Control
+                type="text"
+                placeholder="Kylskåp"
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3" controlId="amount">
               <Form.Label>Antal</Form.Label>
-              <Form.Control type="number" placeholder="3" value={amount} onChange={e => setAmount(parseInt(e.target.value))}/>
+              <Form.Control
+                type="number"
+                placeholder="3"
+                value={amount}
+                onChange={(e) => setAmount(parseInt(e.target.value))}
+              />
             </Form.Group>
             <Form.Group className="mb-3" controlId="unit">
               <Form.Label>Enhet</Form.Label>
-              <Form.Select value={unit} onChange={e => setUnit(e.target.value as Units)}>
+              <Form.Select
+                value={unit}
+                onChange={(e) => setUnit(e.target.value as Units)}
+              >
                 <option value={Units.st}>{Units.st}</option>
                 <option value={Units.kg}>{Units.kg}</option>
                 <option value={Units.g}>{Units.g}</option>
