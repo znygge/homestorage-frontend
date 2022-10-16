dbd8af1a20f55067fdf3f9edbc4c8c3c1e871ed4
Johan Nygren <johan.nygren@knowit.se>
2022-10-16 12:25:38 +0200

    Update app, added fruit fall and rearranged components

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,65 @@
-import './App.css';
-import { useState } from "react";
-import { Col, Container, Row } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AddForm from './components/addForm/AddForm';
-import Products from './components/Products/Products';
-import { Product } from './datastructures';
-import Search from './components/Search/Search';
+import "./App.css";
+import { useState, useRef } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AddForm from "./components/addForm/AddForm";
+import Products from "./components/Products/Products";
+import { Product } from "./datastructures";
+import Search from "./components/Search/Search";
+import FruitFall from "./components/FruitFall/FruitFall";
 
 function App() {
   const [itemList, setItemList] = useState([] as Array<Product>);
-  if(itemList.length == 0)
-  fetch("http://server.nygge.eu:4000/graphql?", {
+  const [itemToEdit, setItemToEdit] = useState(undefined as Product|undefined);
+  const [showAnimation, setShowAnimation] = useState(true);
+
+  setTimeout(()=>{setShowAnimation(false)}, 4000);
+  const openProduct = (item:Product)=>{
+    setItemToEdit(item);
+  };
+  if (itemList.length == 0)
+    fetch("http://server.nygge.eu:4000/graphql?", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({query: `query {
+      body: JSON.stringify({
+        query: `query {
         multipleItem {
           _id,
           name,
           location,
           amount,
-          unit
+          unit,
+          tags
         }
-      }`})}).then((response) => response.json())
+      }`,
+      }),
+    })
+      .then((response) => response.json())
       .then((data) => {
-        if(data.data.multipleItem)
-         setItemList(data.data.multipleItem);
+        if (data.data.multipleItem) setItemList(data.data.multipleItem);
       });
   return (
     <div className="App">
       <header className="App-header">
-        <img src="/assets/Logo.svg" alt="Logga"/>
-        <div className='App-header-arch' />
+        <img src="/assets/Logo.svg" alt="Logga" />
+        <div className="App-header-arch" />
       </header>
       <Search setList={setItemList} />
       <Container>
         <Row>
-        <Col></Col>
-        <Col></Col>
-        <Col><AddForm /></Col>
-      </Row>
-          <Row>
-            <Products data={itemList}/>
-          </Row>
-        </Container>
+          <Products onClick={openProduct} data={itemList} />
+        </Row>
+        <Row className="pt-3">
+        </Row>
+      </Container>
+      <footer style={{position:"sticky", bottom:"5px"}}>
+        <AddForm loadData={itemToEdit} clearLoaded={()=>setItemToEdit(undefined)}/>
+      </footer>
+      {
+       showAnimation && <FruitFall />
+      }
     </div>
   );
 }
